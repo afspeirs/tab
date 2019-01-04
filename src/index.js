2453e6c9583dff6d8b7f32a541aaf9663a2d4fff
Andy <andy.speirs2@gmail.com>
2019-01-04 19:20:50 +0000

    Updated to allow for more than one tab group on the page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 const Tab = (function Tab() {
 	const tabButtons = [...document.querySelectorAll('[data-tab]')];
-	const tabContent = [...document.querySelectorAll('.tab-content')];
 
 	function buttonPress() {
+		const children = [...this.closest('.tab-container').children];
+		// Get the children of the child with a class of tab-buttons
+		const buttons = [...children.filter(child => child.classList.contains('tab-buttons'))[0].children];
+		// Get all the children with a class of tab-content
+		const content = children.filter(child => child.classList.contains('tab-content'));
+
 		// Toggle tab content if the clicked tab is not active
 		if (!this.classList.contains('active')) {
 			// Set clicked tab as active
 			// Remove active from the other tabs
-			tabButtons.map((tab) => (tab === this ? tab.classList.add('active') : tab.classList.remove('active')));
-			tabContent.map((tab) => (tab.id === this.dataset.tab ? tab.classList.add('active') : tab.classList.remove('active')));
+			buttons.map((tab) => (tab === this ? tab.classList.add('active') : tab.classList.remove('active')));
+			content.map((tab) => (tab.id === this.dataset.tab ? tab.classList.add('active') : tab.classList.remove('active')));
 		}
 	}
 
