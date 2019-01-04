0529095f76d85eaf5b6752ea1a95adfe37c988bd
Andy <andy.speirs2@gmail.com>
2019-01-04 18:25:56 +0000

    Updated module to not be an object

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-const Tab = {
-	tabButtons: [...document.querySelectorAll('[data-tab]')],
-	tabContent: [...document.querySelectorAll('.tab-content')],
+const Tab = (function Tab() {
+	const tabButtons = [...document.querySelectorAll('[data-tab]')];
+	const tabContent = [...document.querySelectorAll('.tab-content')];
 
-	init() {
-		this.tabButtons.forEach((e) => {
+	function init() {
+		tabButtons.forEach((e) => {
 			e.addEventListener('click', () => {
 				// Toggle tab content if the clicked tab is not active
 				if (!e.classList.contains('active')) {
 					// Set clicked tab as active
 					// Remove active from the other tabs
-					this.tabButtons.map((tab) => (tab === e ? tab.classList.add('active') : tab.classList.remove('active')));
-					this.tabContent.map((tab) => (tab.id === e.dataset.tab ? tab.classList.add('active') : tab.classList.remove('active')));
+					tabButtons.map((tab) => (tab === e ? tab.classList.add('active') : tab.classList.remove('active')));
+					tabContent.map((tab) => (tab.id === e.dataset.tab ? tab.classList.add('active') : tab.classList.remove('active')));
 				}
 			});
 		});
-	},
-};
+	}
+
+	return { init };
+}());
 
 export default Tab;
